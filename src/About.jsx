994465d4e384c7501d074eb43207525c7e61c226
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-30 11:28:01 -0400

    2 files modified

--- src/About.jsx
+++ src/About.jsx
@@ -9,19 +9,34 @@ const About = () =>{
       <p>
         <strong>
           <em>
-            Hey, welcome to the funny game l ratio it um do the stuff that do the thingy hhahahahhahahahahahaahahah.
+            Hey, welcome to my scuffed roblox page clone ig
           </em>
         </strong>
       </p>
 
       <MakeTBlackL />
 
-      <section id="about-low">
-        <h1>Active</h1>
-        <h4 id="about-low-number">1M</h4>
+      <section>
+        <h1 id="about-low">Active</h1>
+        <h4 id="about-low-number">1M+</h4>
 
-        <h1>Active</h1>
-        <h4 id="about-low-number">1M</h4>
+        <h1 id="about-low">Favorites</h1>
+        <h4 id="about-low-number">100k+</h4>
+
+        <h1 id="about-low">Visits</h1>
+        <h4 id="about-low-number">10Mk+</h4>
+
+        <h1 id="about-low">Created</h1>
+        <h4 id="about-low-number">6/30/22</h4>
+
+        <h1 id="about-low">Updated</h1>
+        <h4 id="about-low-number">6/30/22</h4>
+
+        <h1 id="about-low">Sever Size</h1>
+        <h4 id="about-low-number">12</h4>
+
+        <h1 id="about-low">Genre</h1>
+        <h4 id="about-low-number">Adventure</h4>
       </section>
     </section>
   )
