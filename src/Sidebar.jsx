766b773d09d2495482204cdb44c57d1cef3f9643
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-29 22:54:53 -0400

    2 files modified

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,11 +6,12 @@ import { useState } from "react"
 const Sidebar = () =>{
   const [title,setTitle] = useState("Boubacar's Game")
   const [author,setAuthor] = useState("Boubacar")
+
   return(
     <section id="sidebar">
-      <h1>Img</h1>
       <h1>{title}</h1>
       <h4 id="author">By: {author}</h4>
+      <button id="play">Play</button>
     </section>
   )
 }
