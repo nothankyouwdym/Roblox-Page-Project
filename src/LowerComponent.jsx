fb7acc5970705e713ad2da25a7ba8624f23f9684
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-30 06:20:20 -0400

    1 file modified

--- src/LowerComponent.jsx
+++ src/LowerComponent.jsx
@@ -5,8 +5,8 @@ const LowerComponent = () =>{
   return(
     <section id="lower">
       <h1><Link to="/About" id="lower-links">About</Link></h1>
-      <h1><Link to="/About" id="lower-links">About</Link></h1>
-      <h1><Link to="/About" id="lower-links">About</Link></h1>
+      <h1><Link to="/About" id="lower-links">Store</Link></h1>
+      <h1><Link to="/About" id="lower-links">Servers</Link></h1>
     </section>
   )
 }
