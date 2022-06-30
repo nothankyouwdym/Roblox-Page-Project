5f858f838f42ac5fb15cff5da372ec1bf4da59b1
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-30 06:25:19 -0400

    2 files modified

--- src/LowerComponent.jsx
+++ src/LowerComponent.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const LowerComponent = () =>{
   return(
     <section id="lower">
-      <h1><Link to="/About" id="lower-links">About</Link></h1>
+      <h1><Link to="/About" id="lower-links-first">About</Link></h1>
       <h1><Link to="/About" id="lower-links">Store</Link></h1>
       <h1><Link to="/About" id="lower-links">Servers</Link></h1>
     </section>
